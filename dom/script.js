d258e5a39a064aefd9e2bdba0775229c70637d8b
Abhishek Kumar Mishra <153258500+abhimishra17@users.noreply.github.com>
2025-01-13 20:47:47 +0530

    Add files via upload

---
+++ dom/script.js
@@ -0,0 +1,7 @@
+
+let heading=document.getElementById("title");
+heading.innerHTML="Dom learning from chai and code";
+heading.style.backgroundColor="Red";
+let allhead=document.querySelectorAll('.head');
+allhead.forEach((element)=>
+element.style.backgroundColor="yellow");
